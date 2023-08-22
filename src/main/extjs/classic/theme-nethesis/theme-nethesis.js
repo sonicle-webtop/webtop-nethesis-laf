ebe2740e45d16baeb0848922269da29b1c26f2d6
Matteo Albinola <malbinola@sonicle.com>
2023-08-22 15:56:18 +0200

    Update theme

---
+++ src/main/extjs/classic/theme-nethesis/theme-nethesis.js
@@ -0,0 +1,5 @@
+Ext.namespace('Ext.theme.is').Neptune = true;
+Ext.namespace('Ext.theme.is').Triton = true;
+Ext.namespace('Ext.theme.is').Nethesis = true;
+Ext.theme.name = 'Nethesis';
+Ext.theme.hierarchy = ['Nethesis', 'Triton', 'Neptune'];

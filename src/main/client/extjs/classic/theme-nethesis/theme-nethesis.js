e4718c92c00e4a5732349a82895d1275c3c91b72
Matteo Albinola <malbinola@sonicle.com>
2023-11-22 16:45:57 +0100

    Add taskbar button ui

--- src/main/client/extjs/classic/theme-nethesis/theme-nethesis.js
+++ src/main/client/extjs/classic/theme-nethesis/theme-nethesis.js
@@ -6,8 +6,9 @@ Ext.define('Ext.theme.nethesis.Component', {
 	Ext.namespace('Ext.theme.is').Nethesis = true;
 	Ext.theme.name = 'Nethesis';
 	Ext.theme.hierarchy = ['Nethesis', 'Triton', 'Neptune'];
+	Ext.namespace('Ext.theme.ui.button').toolbar = 'default-toolbar';
 	Ext.namespace('Ext.theme.ui.button').primary = 'default';
 	Ext.namespace('Ext.theme.ui.button').secondary = 'button-secondary';
 	Ext.namespace('Ext.theme.ui.button').tertiary = 'button-tertiary';
-	Ext.namespace('Ext.theme.ui.button').toolbar = 'default-toolbar';
+	Ext.namespace('Ext.theme.ui.button').taskbar = 'button-taskbar';
 });

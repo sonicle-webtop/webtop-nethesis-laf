a26652979ef4b6620f15914019ab1f21b328d886
Matteo Albinola <malbinola@sonicle.com>
2024-07-16 12:40:59 +0200

    New tasks ui

--- src/main/com.sonicle.webtop.tasks/laf/nethesis/override.js
+++ src/main/com.sonicle.webtop.tasks/laf/nethesis/override.js
@@ -13,27 +13,59 @@ Ext.define('Nethesis.overrides.webtop.tasks.Service', {
 	privates: {
 		createGridConfig: function(tagsStore, nest, cfg) {
 			return Ext.apply(this.callParent(arguments), {
-				componentCls: 'wttasks-main-grid',
 				cls: 'x-grid-rounded'
 			});
 		}
 	}
 });
-Ext.define('Nethesis.overrides.webtop.tasks.view.TaskNew', {
-	override: 'Sonicle.webtop.tasks.view.TaskNew',
+Ext.define('Nethesis.overrides.webtop.tasks.view.Task', {
+	override: 'Sonicle.webtop.tasks.view.Task',
 	
-	privates: {
-		createTopToolbar2Cfg: function(items) {
-			// No items are empty by default, so provide our elements!
-			var me = this;
-			return me.createTopToolbarXCfg([
+	/**
+	 * Override original {@link WTA.sdk.ModelView#initTBar}
+	 * - Add private field in 2nd position
+	 * - Add tags field in 3rd position
+	 * - Add bottom divider
+	 */
+	initTBar: function() {
+		var me = this,
+			SoU = Sonicle.Utils;
+		
+		me.dockedItems = SoU.mergeDockedItems(me.dockedItems, 'top', [
+			me.createTopToolbar1Cfg(me.prepareTopToolbarItems()),
+			me.createTopToolbarXCfg([
 				{
 					xtype: 'checkbox',
 					bind: '{isPrivate}',
 					hideEmptyLabel: true,
 					boxLabel: me.res('task.fld-private.lbl')
 				}
-			]);
+			]),
+			me.createTopToolbarXCfg([
+				me.createTagsFieldCfg()
+			], 'last'),
+			me.createTopToolbarsDividerCfg()
+		]);
+		me.dockedItems = SoU.mergeDockedItems(me.dockedItems, 'bottom', [
+			me.createStatusbarCfg()
+		]);
+	},
+	
+	privates: {
+		/**
+		 * Override original {@link Sonicle.webtop.calendar.view.Event#prepareMainFields}
+		 * - Remove tags field from returned items: it's the 1st field added by createTagsFieldCfg() in original impl.
+		 */
+		prepareMainFields: function() {
+			return Ext.Array.slice(this.callParent(arguments), 1);
+		},
+		
+		/**
+		 * Override original {@link Sonicle.webtop.calendar.view.Event#prepareTopToolbarItems}
+		 * - Remove busy and private fields: they are the last 3 items must be removed from the original impl.
+		 */
+		prepareTopToolbarItems: function() {
+			return Ext.Array.slice(this.callParent(arguments), 0, -2);
 		}
 	}
 });
